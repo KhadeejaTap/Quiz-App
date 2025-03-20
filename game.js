445882f65c57cea736b9f38c849a4cdf977b1f33
KhadeejaTap <143401559+KhadeejaTap@users.noreply.github.com>
2025-03-20 10:23:52 -0500

    Update game.js

--- game.js
+++ game.js
@@ -9,24 +9,46 @@ let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-let questions = [];
 
-fetch('questions.json')
-    .then((res) => {
-        return res.json();
-    })
-    .then((loadedQuestions) => {
-        console.log(loadedQuestions);
-        questions = loadedQuestions;
-        startGame();
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+// Hardcoded questions instead of fetching from JSON
+let questions = [
+    {
+      "question": "Inside which HTML element do we put the JavaScript?",
+      "choice1": "<script>",
+      "choice2": "<javascript>",
+      "choice3": "<js>",
+      "choice4": "<scripting>",
+      "answer": 1
+    },
+    {
+      "question": "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      "choice1": "<script href='xxx.js'>",
+      "choice2": "<script name='xxx.js'>",
+      "choice3": "<script src='xxx.js'>",
+      "choice4": "<script file='xxx.js'>",
+      "answer": 3
+    },
+    {
+      "question": "How do you write 'Hello World' in an alert box?",
+      "choice1": "msgBox('Hello World');",
+      "choice2": "alertBox('Hello World');",
+      "choice3": "msg('Hello World');",
+      "choice4": "alert('Hello World');",
+      "answer": 4
+    },
+    {
+      "question": "How do you initialize the variable x?",
+      "choice1": "value var = x;",
+      "choice2": "var x = value;",
+      "choice3": "var value = x;",
+      "choice4": "x var = value;",
+      "answer": 2
+    }
+];
 
 //CONSTANTS
 const CORRECT_BONUS = 25;
-const MAX_QUESTIONS = 4;
+const MAX_QUESTIONS = questions.length;
 
 game.classList.remove("hidden");
 loader.classList.add("hidden");
@@ -41,12 +63,10 @@ startGame = () => {
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
-        //go to the end page
         return window.location.assign('/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    //Update the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
@@ -91,4 +111,5 @@ incrementScore = (num) => {
     scoreText.innerText = score;
 };
 
-//startGame();
+// Start the game
+startGame();
